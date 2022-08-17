679a53eaf73340e768df9adacd93f57209ec8467
Ben Pham <benjamin.pham@gmail.com>
2022-08-17 18:09:05 +0200

    Plant form and create

---
+++ app/controllers/plants_controller.rb
@@ -0,0 +1,16 @@
+class PlantsController < ApplicationController
+  def create
+    @garden = Garden.find(params[:garden_id])
+    @plant = Plant.new(plant_params)
+    @plant.garden = @garden
+    @plant.save
+
+    redirect_to garden_path(@garden)
+  end
+
+  private
+
+  def plant_params
+    params.require(:plant).permit(:name, :image_url)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  resources :gardens
+  resources :gardens do
+    resources :plants, only: [:create]
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
